ee81720851371bc9f1f30a351f662e06644a5c85
vvskchaitanya <vvskchaitanya@gmail.com>
2021-06-13 10:59:35 +0000

    serve

--- scripts/_serve.js
+++ scripts/_serve.js
@@ -1,1 +1,28 @@
-asdf
+var http = require('http');
+var fs=require('fs');
+var _r={recursive:true};
+
+// Deleting target folder
+console.log("Deleting target folder");
+fs.rmdirSync("target",_r)
+// Compiling all files inside src folder
+compile("src");
+
+function compile(dir){
+    fs.readdirSync(dir).forEach(function (file, index) {
+    var path=dir+"/"+file;
+    var stat=fs.statSync(path);
+      if(stat.isDirectory()){
+          tpath=path.replace("src","target");
+          fs.mkdirSync(tpath,_r);
+          compile(path);
+      }else if(stat.isFile()){
+          console.log("Compiling file : "+path);
+          var content=fs.readFileSync(path);
+          path=path.replace("src","target");
+          fs.writeFileSync(path, content);
+      }else{
+          console.log("Unable to compile file : "+path);
+      }
+});
+}
