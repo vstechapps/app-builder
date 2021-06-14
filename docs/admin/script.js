411f19c63bfd7e55ef77640b116f5a4ebb0ffdec
vvskchaitanya <vvskchaitanya@gmail.com>
2021-06-14 19:36:17 +0000

    Added event listener

--- docs/admin/script.js
+++ docs/admin/script.js
@@ -1,1 +1,12 @@
-admin script.js
+var actions=["click"];
+performAction=function(event){
+    console.log(event);
+    console.log("Triggered "+event.type+" on ",event.target);
+    var element=event.target;
+}
+actions.forEach(action=>{
+    $(document).on(action,event=>performAction(event));
+});
+
+
+
