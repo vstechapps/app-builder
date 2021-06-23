9f2b9e96287758fcdd32120122a71ae847ccb2b8
vvskchaitanya <vvskchaitanya@gmail.com>
2021-06-23 06:32:31 +0000

    updated

--- src/admin/firebase.js
+++ src/admin/firebase.js
@@ -9,19 +9,3 @@ var firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
-
-var auth = firebase.auth();
-
-login=function(){
-var provider = new firebase.auth.GoogleAuthProvider();
-provider.addScope('profile');
-provider.addScope('email');
-auth.signInWithPopup(provider).then(function(result) {
- // This gives you a Google Access Token.
- var token = result.credential.accessToken;
- // The signed-in user info.
- var user = result.user;
- console.log(user);
- data.user=user;
-});
-}
--- target/admin/script.js
+++ target/admin/script.js
@@ -1,11 +1,18 @@
 import Page from "./main.js";
 
-var app=new Page();
+var app = new Page();
 
-app.data.menu=[{name:"login",action:"login"}];
-app.methods.login=function(d){
-    console.log("Hello login");
-    console.log(d);
-    app.data.menu=[{name:"home",action:"home"}];
+app.data.menu = [{ name: "login", action: "login" }];
+app.methods.login = function (d) {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('profile');
+    provider.addScope('email');
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        console.log("User Logged In : ",user);
+        data.user = user;
+    });
 }
-//app.render();
